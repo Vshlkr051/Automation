48144f52e559a4cc9644064b843dd74b1fc349f0
Vshlkr051 <vshlkumar051@gmail.com>
2023-04-21 13:03:32 +0900

    convert language of any excel file

--- LanguageConversion.py
+++ LanguageConversion.py
@@ -1,8 +1,17 @@
+# Python code to convert excel file from one language to other
+# User need to maintain master file where column_1 contains original word and column_2 contains translated word
+# ===================================================================================
+# Before executing file, user need to install "openpyxl" package into their system
+# >>> pip install openpyxl
+#  for some user they might have pip3 installed on the system , so they need to run as
+# >>> pip3 install openpyxl
+# =====================================================================================
+
 import openpyxl
 
 dictionary = input("enter the dictionary file path : ")
 file = input("enter the file path to translate : ")
-target_file = input("enter the destination path with file name : ")
+target_file = input("enter the destination path along with file name : ")
 
 # Load "xlsx file 1"
 wb1 = openpyxl.load_workbook(dictionary)
@@ -12,12 +21,12 @@ sheet1 = wb1.active
 wb2 = openpyxl.load_workbook(file)
 sheet2 = wb2.active
 
-# Create a dictionary to store the translation mappings from "xlsx file 1"
+# Create a dictionary to store the translation mappings from "xlsx file 1" as key value pair {key : value}
 translation_dict = {}
 for row in sheet1.iter_rows(values_only=True):
     translation_dict[row[0]] = row[1]
 
-# Translate data in "xlsx file 2" based on the mappings
+# Translate data in "xlsx file 2" based on the mappings, it will do row by row translation
 for row in sheet2.iter_rows():
     for cell in row:
         if cell.value in translation_dict:
